035989f3305d6ab450c1bf480fdfbd4a4662f97f
alberto <albertozanola1996@gmail.com>
2021-07-21 14:16:40 +0200

    snack 1 - 2 - 3

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//SNACK 1
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+// var numero = parseInt(prompt("inserisci un numero"));
+
+// if (numero % 2 == 0) {
+//     console.log(numero);
+// }
+// else {
+//     numero = numero + 1;
+//     console.log(numero);
+// }
+
+
+//SNACK 2
+//Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+// var nome = ["alberto", "luca", "stefano", "leonardo",];
+// var cognome = ["rossi", "verdi", "ferri", "bianchi"];
+// var random;
+// var random_2;
+
+// for (var i = 0; i < 4; i++) {
+//     random = Math.floor(Math.random()*nome.length);
+//     random_2 = Math.floor(Math.random()*cognome.length);
+//     console.log((nome[random]) + (" ") + (cognome[random_2]));
+// }
+
+
+//SNACK 3
+//Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+// var numeri = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// somma = 0;
+// dispari = 0;
+
+// for (var i = 0; i < numeri.length; i++) {
+// 	dispari = numeri[i];
+
+// 	if (dispari % 2 != 0) {
+// 		somma = somma + dispari;
+// 	}
+// }
+// console.log(somma);
+
